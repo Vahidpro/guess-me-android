b4cd5a09e384f9802be9a5301afdcc0898a15d13
Vahid Porki <ivahidp@gmail.com>
2023-01-22 17:28:19 +0330

    Added Gradient background

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import StartGameScreen from "./screens/StartGameScreen";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function App() {
 	return (
-		<View style={styles.container}>
+		<LinearGradient colors={["#060635", "#0000c0"]} style={styles.container}>
 			<StatusBar style="light" />
 			<StartGameScreen></StartGameScreen>
-		</View>
+		</LinearGradient>
 	);
 }
 
