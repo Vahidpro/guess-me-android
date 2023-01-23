7ea14c13c57e054b75c78c4872bdbd918ed778c1
Vahid Porki <ivahidp@gmail.com>
2023-01-23 15:18:23 +0330

    Styles GameOver Page

--- constants/colors.js
+++ constants/colors.js
@@ -1,6 +1,7 @@
 const Colors = {
 	primary: "#09296e",
 	accent100: "#ff4800ff",
+	accent800: "#160300ff",
 	question: "#fffb00ff",
 	oponentGuessTitle: "#00ffff",
 };
--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,7 +1,41 @@
-import { Text } from "react-native";
+import { Image, StyleSheet, View } from "react-native";
+import Title from "../components/ui/Title";
+import Colors from "../constants/colors";
 
 function GameOverScreen() {
-	return <Text>Game is over</Text>;
+	return (
+		<View style={styles.rootContainer}>
+			<Title>Game is over</Title>
+			<View style={styles.imageContainer}>
+				<Image
+					style={styles.image}
+					source={require("../assets/images/success.png")}
+				></Image>
+			</View>
+		</View>
+	);
 }
 
 export default GameOverScreen;
+
+const styles = StyleSheet.create({
+	rootContainer: {
+		flex: 1,
+		padding: 24,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	imageContainer: {
+		width: 300,
+		height: 300,
+		borderRadius: 150,
+		borderWidth: 3,
+		borderColor: Colors.accent800,
+		overflow: "hidden",
+		margin: 36,
+	},
+	image: {
+		width: "100%",
+		height: "100%",
+	},
+});
